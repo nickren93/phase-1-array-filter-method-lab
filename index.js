6d06127e0942d53c1f004dbf5c649cea7cd839bd
Sitong <nickren93@gmail.com>
2024-11-10 19:47:01 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(array, string){
+    function findName(element){
+        return string.toUpperCase() == element.toUpperCase()
+    }
+    return array.filter(findName)
+}
+
+function fuzzyMatch(array, string){
+    function charCompare(element){
+        let newElement = element.slice(0,string.length)
+        return newElement.toUpperCase() == string.toUpperCase()
+    }
+    return array.filter(charCompare)
+}
+
+function matchName(array, string){
+    function findName(element){
+        return string.toUpperCase() == element.name.toUpperCase()
+    }
+    return array.filter(findName)
+}
+
